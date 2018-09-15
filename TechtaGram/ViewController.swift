8df119ee9cc5d0e72df1667fdc3e1da6566f9993
miyass <exe.miyakura0629@gmail.com>
2018-09-15 22:43:27 +0900

    DONE

--- TechtaGram/ViewController.swift
+++ TechtaGram/ViewController.swift
@@ -7,19 +7,90 @@
 //
 
 import UIKit
+import Accounts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var cameraImageView: UIImageView!
+    
+    var originalImage: UIImage!
+    var filter: CIFilter!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func useCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            
+            //カメラの起動
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+        } else {
+            print("error")
+        }
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        cameraImageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        originalImage = cameraImageView.image
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func applyFilter(_ sender: Any) {
+        let filterImage = CIImage(image: originalImage)!
+        
+        filter = CIFilter(name: "CIColorControls")!
+        filter.setValue(filterImage, forKey: kCIInputImageKey)
+        
+        filter.setValue(1.0, forKey: "inputSaturation")
+        filter.setValue(0.5, forKey: "inputBrightness")
+        filter.setValue(2.5, forKey: "inputContrast")
+        
+        let ctx = CIContext(options: nil)
+        let cgImage = ctx.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+        cameraImageView.image = UIImage(cgImage: cgImage!)
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        UIImageWriteToSavedPhotosAlbum(cameraImageView.image!, nil, nil, nil)
+    }
+    
+    @IBAction func openAlbum(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func share(_ sender: Any) {
+        let shareText = "写真加工できた！"
+        
+        let shareImage = cameraImageView.image!
+        
+        let activityItems: [Any] = [shareText, shareImage]
+        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        
+        let excludeActivityTypes = [UIActivityType.postToWeibo, .saveToCameraRoll, .print]
+        activityViewController.excludedActivityTypes = excludeActivityTypes
+        
+        present(activityViewController, animated: true, completion: nil)
+        
+    }
+    
+    
+    
 }
 
